c93ecec787ae080ec6ff0fba0c312a30279f8fbb
Alon Landmann <rafael.landmann@gmail.com>
2022-05-16 13:38:04 +0200

    FIX google analytics try 2

--- pages/_document.js
+++ pages/_document.js
@@ -9,20 +9,16 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap' />
 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3VCPTW9716"></Script>
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={
-            {
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-3VCPTW9716');
-              `
-            }
-          }
-        </Head>
+        <Script async src='https://www.googletagmanager.com/gtag/js?id=G-3VCPTW9716'></Script>
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3VCPTW9716');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />

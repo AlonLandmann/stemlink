04418970641cbab22f26f709605d5b355e534881
Alon Landmann <rafael.landmann@gmail.com>
2022-05-16 13:25:26 +0200

    ADD google analytics (first try)

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,21 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap' />
-      </Head>
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3VCPTW9716"></Script>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={
+            {
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-3VCPTW9716');
+              `
+            }
+          }
+        </Head>
       <body>
         <Main />
         <NextScript />
